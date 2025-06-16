b2bebfea0fc5fb78cc73f451c307a8e577e093ea
prammmoe <ikhwanpramuditha05@gmail.com>
2025-06-16 09:51:44 +0700

    Initial Commit

---
+++ Facy/FacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacyApp.swift
+//  Facy
+//
+//  Created by Pramuditha Muhammad Ikhwan on 16/06/25.
+//
+
+import SwiftUI
+
+@main
+struct FacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacyTests/FacyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FacyTests.swift
+//  FacyTests
+//
+//  Created by Pramuditha Muhammad Ikhwan on 16/06/25.
+//
+
+import Testing
+@testable import Facy
+
+struct FacyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
