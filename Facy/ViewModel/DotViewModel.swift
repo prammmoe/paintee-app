2cdb2444872c00cdc40c9d6468eb2339e866109b
prammmoe <ikhwanpramuditha05@gmail.com>
2025-06-23 10:26:25 +0700

    feat: refactor structure

---
+++ Facy/ViewModel/DotViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  DotViewModel.swift
+//  Facy
+//
+//  Created by Pramuditha Muhammad Ikhwan on 22/06/25.
+//
+
+import SwiftUI
+
+struct DotViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DotViewModel()
+}
