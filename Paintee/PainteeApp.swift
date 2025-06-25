f3bae19bd063e7461e80dfcf02c00702abfa5e97
prammmoe <ikhwanpramuditha05@gmail.com>
2025-06-25 17:00:42 +0700

    Initial Commit

---
+++ Paintee/PainteeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PainteeApp.swift
+//  Paintee
+//
+//  Created by Pramuditha Muhammad Ikhwan on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PainteeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PainteeTests/PainteeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PainteeTests.swift
+//  PainteeTests
+//
+//  Created by Pramuditha Muhammad Ikhwan on 25/06/25.
+//
+
+import Testing
+@testable import Paintee
+
+struct PainteeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
